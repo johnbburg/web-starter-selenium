d04408fe15219993912670622cc27c2752ba08dc
John Brandenburg <jbrandenburg@forumone.com>
2015-05-26 10:30:17 -0400

    Removed extra $'s.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@ class web-starter-selenium ($version = "2.45.0") {
   $major_version = $version_array[0]
   $minor_version = $version_array[1]
   $release_version = $version_array[2]
-  $url = "http://selenium-release.storage.googleapis.com/${$major_version}.${$minor_version}/selenium-server-standalone-${major_version}.${$minor_version}.${$release_version}.jar"
+  $url = "http://selenium-release.storage.googleapis.com/${major_version}.${minor_version}/selenium-server-standalone-${major_version}.${minor_version}.${release_version}.jar"
   $filename = 'selenium-$version'
 
   package { "xvfb":
