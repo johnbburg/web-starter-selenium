26841c4dcce6f8dfa31bec285b5325fc7a047ead
John Brandenburg <jbrandenburg@forumone.com>
2015-05-26 15:47:28 -0400

    Fixes filename uses.

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@ class web-starter-selenium::selenium($version = "2.45.0"){
   $minor_version = $version_array[1]
   $release_version = $version_array[2]
   $url = "http://selenium-release.storage.googleapis.com/${major_version}.${minor_version}/selenium-server-standalone-${major_version}.${minor_version}.${release_version}.jar"
-  $filename = "selenium-server-standalone-${version}"
+  $filename = "selenium-server-standalone-${version}.jar"
 
   package { "xorg-x11-server-Xvfb":
     ensure => installed,
@@ -21,15 +21,15 @@ class web-starter-selenium::selenium($version = "2.45.0"){
     command => "wget --directory-prefix=/tmp/vagrant-cache ${url}",
     path    => '/usr/bin',
     require => [ Package["java-1.7.0-openjdk"], File["/tmp/vagrant-cache"] ],
-    creates => "/tmp/vagrant-cache/${filename}.tgz",
+    creates => "/tmp/vagrant-cache/${filename}",
     timeout => 4800,
   }
 
   # extract from the selenium archive
-  exec { "web-starter-selenium::extract":
-    command => "tar -zxvf /tmp/vagrant-cache/${filename}.tgz -C /opt",
+  exec { "web-starter-selenium::move":
+    command => "cp /tmp/vagrant-cache/${filename} /opt",
     path    => ["/bin"],
     require => [Exec["web-starter-selenium::download"]],
-    creates => "/opt/selenium-server-standalone-${version}.jar",
+    creates => "/opt/${filename}",
   }
 }
